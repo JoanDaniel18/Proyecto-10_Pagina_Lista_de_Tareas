00307ed4e8f7b29da4c446a740eb12b6fb3b02bb
Daniel Rivas <71899829+JoanDaniel18@users.noreply.github.com>
2024-03-11 02:41:37 -0500

    Agregamos los docs HTML, CSS y JS

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector(".click").onclick = function(){
+    if(document.querySelector(".newitem input").value.length == 0){
+        alert('Por favor escribe algo...')
+    }else{
+        document.querySelector("#tag").innerHTML += `
+        <div class= "list">
+            <span id="listname">
+                ${document.querySelector(".newitem input").value}
+            </span>
+            <button class="delete">
+                <i class='bx bx-message-square-x'></i>
+            </button>
+        </div>
+        `;
+        var current_list = document.querySelectorAll(".delete");
+        for(var i=0; i<current_list.length;i++){
+            current_list[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+        
+        var list = document.querySelector(".list");
+        for(var i=0; i<list.lenght;i++){
+            list[i].onclick = function(){
+                this.classlist.toggle("comple")
+            }
+        }
+        document.querySelector(".newitem input").value = "";
+    }
+}
